870a78646c1708cd3210ef6e0548fdb22ad34401
indra55 <galahitanshu@gmail.com>
2026-09-08 01:08:59 +0530

    trippy asl

---
+++ fractal.go
@@ -0,0 +1,76 @@
+package main 
+import (
+	"math"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+type Box struct {
+	MinX float64
+	MaxX float64
+	MinY float64
+	MaxY float64
+}
+
+type Segment struct {
+	A Point
+	B Point
+}
+
+func segmentOutsideBox(a Point, b Point, box Box) bool {
+	segMinX := math.Min(a.X, b.X)
+	segMaxX := math.Max(a.X, b.X)
+	segMinY := math.Min(a.Y, b.Y)
+	segMaxY := math.Max(a.Y, b.Y)
+
+	if segMaxX < box.MinX || segMinX > box.MaxX || segMaxY < box.MinY || segMinY > box.MaxY {
+		return true
+	}
+	return false
+}
+
+func generateSegment(a Point, b Point, depth int, box Box, result *[]Segment)  {
+	if depth==0 {
+		*result=append(*result, Segment{A:a,B:b})
+		return
+	}else{
+		if segmentOutsideBox(a,b,box) {
+			return
+		}
+		
+		p1:= Point{
+			a.X+(1.0/3.0)*(b.X-a.X), 
+			a.Y+(1.0/3.0)*(b.Y-a.Y)}
+		p2:= Point{
+			b.X+(1.0/3.0)*(a.X-b.X),b.Y+(1.0/3.0)*(a.Y-b.Y)}
+		
+		dx:= p2.X-p1.X
+		dy:= p2.Y-p1.Y
+		angle:= 60*math.Pi/180
+		dx_rot:=dx*math.Cos(angle)-dy*math.Sin(angle)
+		dy_rot:=dx*math.Sin(angle)+dy*math.Cos(angle)
+
+		p3:= Point{
+			p1.X+dx_rot, p1.Y+dy_rot}
+		
+		generateSegment(a,p1,depth-1,box,result)
+		generateSegment(p1,p3,depth-1,box,result)
+		generateSegment(p3,p2,depth-1,box,result)
+		generateSegment(p2,b,depth-1,box,result)
+		
+	}
+}
+
+func depthForZoonm(zoom float64) int {
+	depth:=int(math.Log(zoom)/math.Log(3))
+	depth+=2
+	if depth>8 {
+		depth=8
+	} else if depth<1 {
+		depth=1
+	}
+	return depth
+}
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func snowFlakeHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	
+	p1 := Point{0, 100}
+	p2 := Point{-100, -50}
+	p3 := Point{100, -50}
+
+	zoom,err := strconv.ParseFloat(r.URL.Query().Get("zoom"),64)
+	if err!=nil {
+		zoom=1
+	}
+	depth:=depthForZoonm(zoom)
+
+	centerX,err := strconv.ParseFloat(r.URL.Query().Get("centerX"),64)
+	if err!=nil {
+		centerX=0
+	}
+	centerY,err := strconv.ParseFloat(r.URL.Query().Get("centerY"),64)
+	if err!=nil {
+		centerY=0
+	}
+
+	baseWidth:=300.0
+
+	visibleWidth:= baseWidth/zoom
+
+	box:= Box{
+		centerX-visibleWidth/2,
+		centerX+visibleWidth/2,
+		centerY-visibleWidth/2,
+		centerY+visibleWidth/2,
+	}
+	
+
+	result := []Segment{}
+
+	generateSegment(p1, p2, depth, box, &result)
+	generateSegment(p2, p3, depth, box, &result)
+	generateSegment(p3, p1, depth, box, &result)
+
+	fmt.Println("result",len(result))
+
+	json.NewEncoder(w).Encode(result)	
+
+}
+
+func main(){
+	fmt.Println("fractals are cool")
+ 	http.HandleFunc("/snowflake",snowFlakeHandler)
+    http.ListenAndServe(":8080",nil)
+}
